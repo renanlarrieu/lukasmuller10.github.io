a755dbc7a16b086d46ac30abf1fa7e6b3f6ba779
muller <lukasmullerdeoliveira@gmail.com>
2020-08-03 00:55:09 -0300

    added the waves

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,16 +1,17 @@
 var width, height, center;
-var points = 10;
+var points = 20;
 var smooth = true;
 var path = new Path();
 var mousePos = view.center / 2;
-var pathHeight = mousePos.y;
-path.fillColor = 'black';
+var pathHeight = mousePos.y/10;
+path.fillColor = 'darkblue';
 initializePath();
 
+//WAVES 
 function initializePath() {
     center = view.center;
     width = view.size.width;
-    height = view.size.height / 2;
+    height = view.size.height/1.05;
     path.segments = [];
     path.add(view.bounds.bottomLeft);
     for (var i = 1; i < points; i++) {
@@ -18,14 +19,13 @@ function initializePath() {
         path.add(point);
     }
     path.add(view.bounds.bottomRight);
-    path.fullySelected = true;
 }
 
 function onFrame(event) {
-    pathHeight += (center.y - mousePos.y - pathHeight) / 10;
+    pathHeight += (center.y - mousePos.y - pathHeight) / 100;
     for (var i = 1; i < points; i++) {
         var sinSeed = event.count + (i + i % 10) * 100;
-        var sinHeight = Math.sin(sinSeed / 200) * pathHeight;
+        var sinHeight = Math.sin(sinSeed / 200) * pathHeight * 0.3;
         var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
         path.segments[i].point.y = yPos;
     }
@@ -53,3 +53,4 @@ function onMouseDown(event) {
 function onResize(event) {
     initializePath();
 }
+//
