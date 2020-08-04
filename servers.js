ad6b3978c877f350b421c1ac2b668205b6c2b3de
muller <lukasmullerdeoliveira@gmail.com>
2020-08-04 14:24:37 -0300

    changed the place of index.html

--- servers.js
+++ servers.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./public/.'));
+app.use(express.static('.'));
 app.listen(8080, () => (console.log('Express server running on port: 8080')));
